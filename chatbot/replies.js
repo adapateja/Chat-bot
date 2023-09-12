639f64f2185db1063ddf8c94b24abd9d1aacf6e1
adapateja <126445884+adapateja@users.noreply.github.com>
2023-09-12 18:56:49 +0530

    Add files via upload

---
+++ chatbot/replies.js
@@ -0,0 +1,13 @@
+const responseObj = {
+    hi:"Hi",
+    Hi:"Hi",
+    Hello: "Hey ! How are you doing ?",
+    hello:"Hey ! How are you doing ?",
+    Hey:"Hey! What's Up",
+    hey: "Hey! What's Up",
+    Date: new Date().toDateString(),
+    Time: new Date().toLocaleTimeString(),
+    Your_creator:"Teja",
+    
+  };
+  
